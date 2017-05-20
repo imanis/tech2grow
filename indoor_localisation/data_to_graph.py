0fae825d0b90c40492f964f75c135189ed15bb0f
Anis Hosni <anis.hosni@cy-play.com>
2017-05-20 16:25:42 +0200

    category to  graph #1

--- indoor_localisation/data_to_graph.py
+++ indoor_localisation/data_to_graph.py
@@ -3,11 +3,17 @@ import pandas as pd
 import numpy as np
 
 
+#########################################################################################
+#   The purpose of this script is to model category data as graph.
+#   This will help us to guide the person through the category graph inside the store
+#########################################################################################
+
+
 #import data as csv into pd dataframe
 categories = pd.read_csv("./data/STORE_CATEGORY_LOCALIZATION.txt", sep="|", header=0,encoding='latin-1')
 
 # lets see some data
-categories.head()
+print(categories.head())
 
 # filter on pilote store FRA118
 categories_FRA118 = categories[categories.STORE_KEY=="FRA118"]
@@ -15,7 +21,7 @@ categories_FRA118 = categories[categories.STORE_KEY=="FRA118"]
 # prepare nodes
 cat_matirix = np.zeros((1000,1000))
 
-# add node to Graph
+# get cats as nodes
 nodes=[]
 for index, row in categories_FRA118.iterrows():
     #print(row['HYP_GRP_CLASS_KEY'], row['HYP_GRP_CLASS_DESC'], row['ABSCISSA'],row['ORDINATE'] )
@@ -23,12 +29,36 @@ for index, row in categories_FRA118.iterrows():
     
 #insert nodes into matrix
 for node in nodes:
-    cat_matirix[node[3]][node[2]]=node[0]
+    cat_matirix[node[3]][node[2]]=node[0]of the
+
+def get_element_matrix_nighboors(rowNumber, columnNumber,matrix):
+    """
+    a function that return an element nighboors in the matrix
+    """
+    radius=1
+    nighboors = [cat_matirix[i][j] if  i >= 0 and i < len(cat_matirix) and j >= 0 and j < len(cat_matirix[0]) else 0 for j in range(columnNumber-radius, columnNumber+radius+1) for i in range(rowNumber-radius, rowNumber+ radius+1)]
+    # delete the central element
+    nighboors.pop(int(len(nighboors)/2)+1)
+    # returnt nighboors ! yea homee
+    return nighboors
+
 
 # construct andd fill the graph :o 
 G=nx.Graph()
+# add edges (implicitly nodes) 
 for node in nodes:
-    G.add_node(node) 
+    # get nbrs 
+    nbrs = get_element_matrix_nighboors(node[2],node[3],cat_matirix)
+    for nbr in nbrs:
+        G.add_edge(node[0],nbr) 
+
+# populate node's data 
+for node in nodes:
+    G[node[0]]['cat'] = node[1]
+    G[node[0]]['abs'] = node[2]
+    G[node[0]]['ord'] = node[3]
+
+
 
 
 
